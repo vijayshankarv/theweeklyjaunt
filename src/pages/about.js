0bde5a89cf605c879a867c0bae4ae3772706a87e
Vijay Shankar Venkataraman <vijay@pagerduty.com>
2020-04-22 22:59:42 -0400

    adding index, about, and a header javascript file

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+    <div style={{ color: `teal` }}>
+    <Header headerText="About The Weekly Jaunt" />
+    <p>Just a random walk</p>
+    </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => <div style={{ color: `purple` }}>
+    <h1>Welcome to the Weekly Jaunt!</h1>
+    <p>Where will we go this time around?</p>
+    <img src="/images/eric-muhr-unsplash.jpg" alt="" />
+
+    </div>
